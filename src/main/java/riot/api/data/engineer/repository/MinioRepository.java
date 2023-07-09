3b14169e60ee08ac79af8c916440270d2f81be73
Ham woojo <woojo3398@naver.com>
2023-07-09 21:22:22 +0900

    [REFACTOR] 파입 얼로드 관련 삭제

--- src/main/java/riot/api/data/engineer/repository/MinioRepository.java
+++
@@ -1,7 +0,0 @@
-package riot.api.data.engineer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import riot.api.data.engineer.entity.MinioInfo;
-
-public interface MinioRepository extends JpaRepository<MinioInfo, Long> {
-}
