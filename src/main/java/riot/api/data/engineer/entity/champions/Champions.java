1511d8582108ea0d146a8ed5857cf9810fea0a73
woojo <woojo3398@datanuri.net>
2023-07-19 18:20:45 +0900

    [REFACTOR] Champions 클래스 생성자 수정 - jsonObject 가져올 때 필요한 스트링 static final 상수 선언

--- src/main/java/riot/api/data/engineer/entity/champions/Champions.java
+++ src/main/java/riot/api/data/engineer/entity/champions/Champions.java
@@ -1,5 +1,6 @@
 package riot.api.data.engineer.entity.champions;
 
+import com.google.gson.JsonObject;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,19 @@ import java.util.List;
 @Setter
 @Getter
 public class Champions {
+    private static final String JSON_OBJECT_VERSION = "version";
+    private static final String JSON_OBJECT_TYPE = "type";
+    private static final String JSON_OBJECT_FORMAT = "format";
+
     private String type;
     private String version;
     private String format;
     private List<Data> dataList;
+
+
+    public Champions(JsonObject jsonObject){
+        this.version = String.valueOf(jsonObject.get(JSON_OBJECT_VERSION));
+        this.type = String.valueOf(jsonObject.get(JSON_OBJECT_TYPE));
+        this.format = String.valueOf(jsonObject.get(JSON_OBJECT_FORMAT));
+    }
 }
