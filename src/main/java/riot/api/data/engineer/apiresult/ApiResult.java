886307a58cd947d4872c56a0fb3aee0f22a0d188
woojo <woojo3398@datanuri.net>
2023-07-19 17:36:53 +0900

    [REFACTOR] API Status Code enum 정의

--- src/main/java/riot/api/data/engineer/apiresult/ApiResult.java
+++ src/main/java/riot/api/data/engineer/apiresult/ApiResult.java
@@ -13,4 +13,26 @@ public class ApiResult {
     String message;
     Object data;
 
+    public enum ApiStatus{
+        OK(200,"success"),
+        CREATED(201,"success"),
+        BAD_REQUEST(400, "fail"),
+        UNAUTHORIZED(401, "fail"),
+        NOT_FOUND(401, "fail"),
+        INTERNAL_SERVER_ERROR(500,"fail"),
+        SERVICE_UNAVAILABLE(503,"fail");
+        Integer statusCode;
+        String status;
+        ApiStatus(int statusCode, String status) {
+            this.status = status;
+            this.statusCode = statusCode;
+        }
+        public Integer getStatusCode() {
+            return statusCode;
+        }
+        public String getStatus(){
+            return status;
+        }
+    }
+
 }
