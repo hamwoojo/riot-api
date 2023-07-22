1c18e064d57c6572155dc6a51b72d01e37fc88cc
hamwoojo <woojo3398@naver.com>
2023-07-22 19:52:04 +0900

    [REFACTOR] WebClientDTO 생성 로직 제거

--- src/main/java/riot/api/data/engineer/interfaces/ApiCallHelper.java
+++ src/main/java/riot/api/data/engineer/interfaces/ApiCallHelper.java
@@ -2,15 +2,12 @@ package riot.api.data.engineer.interfaces;
 
 import org.springframework.web.reactive.function.client.WebClient;
 import riot.api.data.engineer.dto.WebClientDTO;
-import riot.api.data.engineer.dto.api.ApiInfo;
 import riot.api.data.engineer.dto.api.ApiKey;
 import riot.api.data.engineer.entity.Version;
 
 import java.util.List;
-import java.util.Map;
 
 public interface ApiCallHelper {
-    WebClientDTO getWebClientDTO(ApiInfo apiInfo, List<String> pathVariable, Map<String, String> queryParams);
 
     List<String> setPathVariableVersion(Version version);
 
--- src/main/java/riot/api/data/engineer/interfaces/ApiCallHelperImpl.java
+++ src/main/java/riot/api/data/engineer/interfaces/ApiCallHelperImpl.java
@@ -1,39 +1,17 @@
 package riot.api.data.engineer.interfaces;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import riot.api.data.engineer.dto.WebClientDTO;
-import riot.api.data.engineer.dto.api.ApiInfo;
 import riot.api.data.engineer.dto.api.ApiKey;
 import riot.api.data.engineer.entity.Version;
 import riot.api.data.engineer.entity.WebClientCaller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Service
-public class ApiCallHelperImpl implements ApiCallHelper{
-    @Override
-    public WebClientDTO getWebClientDTO(ApiInfo apiInfo, List<String> pathVariable, Map<String, String> queryParams){
-        WebClientDTO.WebClientDTOBuilder builder = WebClientDTO.builder().
-                scheme(apiInfo.getApiScheme())
-                .host(apiInfo.getApiHost())
-                .path(apiInfo.getApiUrl());
-
-        boolean pathVariableIsEmpty = pathVariableCheck(pathVariable);
-        if(!pathVariableIsEmpty){
-            builder.pathVariable(pathVariable);
-        }
-
-        boolean queryParamsIsEmpty = queryParamsCheck(queryParams);
-        if(!queryParamsIsEmpty){
-            builder.queryParam(queryParams);
-        }
-
-        return builder.build();
-    }
+public class ApiCallHelperImpl implements ApiCallHelper {
 
     @Override
     public List<String> setPathVariableVersion(Version version) {
@@ -43,20 +21,14 @@ public class ApiCallHelperImpl implements ApiCallHelper{
     }
 
     @Override
-    public Object apiCall(WebClientDTO webClientDTO,WebClient webClient, ApiKey apiKey, Class<?> responseType) {
+    public Object apiCall(WebClientDTO webClientDTO, WebClient webClient, ApiKey apiKey, Class<?> responseType) {
 
         return WebClientCaller.builder()
                 .webClientDTO(webClientDTO)
                 .webclient(webClient)
-                .build().getWebClientData(apiKey,responseType);
+                .build().getWebClientData(apiKey, responseType);
     }
 
-    private boolean pathVariableCheck(List<String> pathVariable) {
-        return CollectionUtils.isEmpty(pathVariable);
-    }
 
-    private boolean queryParamsCheck(Map<String, String> queryParams) {
-        return CollectionUtils.isEmpty(queryParams);
-    }
 }
 
