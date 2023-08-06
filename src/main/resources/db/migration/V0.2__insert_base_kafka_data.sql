d5af8142ab63edc7b63a22d0fcbe62404c8f7f4b
hamwoojo <woojo3398@naver.com>
2023-08-06 23:32:39 +0900

    <feat> insert flyway 추가

---
+++ src/main/resources/db/migration/V0.2__insert_base_kafka_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.kafka_info
+(topic_name, "partition", replicas, api_info_id)
+VALUES('matchs', 5, 3, 13);
+INSERT INTO public.kafka_info
+(topic_name, "partition", replicas, api_info_id)
+VALUES('runes', 5, 3, 17);
+INSERT INTO public.kafka_info
+(topic_name, "partition", replicas, api_info_id)
+VALUES('champions', 5, 3, 15);
+INSERT INTO public.kafka_info
+(topic_name, "partition", replicas, api_info_id)
+VALUES('items', 5, 3, 14);
+INSERT INTO public.kafka_info
+(topic_name, "partition", replicas, api_info_id)
+VALUES('spells', 5, 3, 16);
